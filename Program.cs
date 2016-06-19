4fcbebb49821ba19a0fd614aaf3a3a574a42a888
Mark Ludwig <mk.ludwig1@gmail.com>
2016-06-18 18:16:53 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NativeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
